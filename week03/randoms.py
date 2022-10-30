4d464cdf32d845feffb15f06a8d5e5d4c022c62e
pranithajacob <pranithab777@gmail.com>
2022-10-30 17:22:40 +0530

    randoms.py

---
+++ week03/randoms.py
@@ -0,0 +1,15 @@
+import random
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+print(random.randint(1, 100))    # line 4
+
+
+# What did you see on line 1?
+# the smallest is the 5 and the biggest is 15
+
+# What did you see on line 2?
+# the smallest is the 3 and the largest is the 9
+
+# What did you see on line 3?
+# the smallest number is 2.726730238510555 and the biggest is 5.149647037259231
