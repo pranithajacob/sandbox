4a8e29af189849d7c492b23c6978a8334b7c4ed2
pranithajacob <pranithab777@gmail.com>
2022-11-05 10:11:15 +0530

    list_warmup.py

---
+++ week04/lists_warmup.py
@@ -0,0 +1,21 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+
+
+my_numbers = [30, 1, 4, 6, 5, 2, 7, 9, 1]
+# Change the first element of numbers to "ten"
+print(my_numbers[0])  # = "ten"
+# Change the last element of numbers to 1
+print(my_numbers[-1])  # = 1
+# Get all the elements from numbers except the first two
+print(my_numbers[2:])
+# Check if 9 is an element of numbers
+print(9 in my_numbers)
